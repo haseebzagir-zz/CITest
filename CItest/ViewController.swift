7a98e35f239cee1b4d908d3c5e85c96edaa94588
Mohamed Haseeb <mhaseeb@suyati.com>
2018-07-04 15:34:11 +0530

    initial commit

---
+++ CItest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CItest
+//
+//  Created by Mohamed Haseeb on 04/07/18.
+//  Copyright © 2018 Mohamed Haseeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
