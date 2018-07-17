4f8efeaec8f0fcceb879b49474e9ac698f87febf
Haseeb <mhaseeb@suyati.com>
2018-07-17 16:50:58 +0530

    minor updates

--- CItest/ViewController.swift
+++ CItest/ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+#if INTERNAL
+let isInternalVersion = true
+#else
+let isInternalVersion = false
+#endif
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if isInternalVersion {
+            self.textlabel.text = "Internal version"
+            print("Internal version")
+        }else {
+            self.textlabel.text = "Release version"
+            print("Release version")
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 }
+
 
